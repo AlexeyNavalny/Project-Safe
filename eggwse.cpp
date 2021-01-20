1f1ee4bf973f5a867b5c9525c428f61dc70bcda6
AlexeyNavalny <77383766+AlexeyNavalny@users.noreply.github.com>
2021-01-20 19:20:24 +1000

    Revert "Update eggwse.cpp"
    This reverts commit d78c34cbee3b84ee0259adeb60c12a576c0a8ab9.

--- eggwse.cpp
+++ eggwse.cpp
@@ -1,9 +1,48 @@
-#include <iostream>;
+gdsgdsgsg
+asffasasfsafasf
 
-using namespace std;
+fsaasfasf
 
-void main() {
-    cout << "привет";
-    string name;
-    cin >> name;
+
+falseasf
+falses
+
+f
+__cpp_alias_templatesf
+aseqw
+edwsqaseqw
+ewqe
+decltypeqwq
+rd\qw
+else if (qwe
+wqwq
+e
+{{{ qw
+qwew
+q
+w
+x
+asd
+
+as
+
+asew
+qrt
+yqe
+yw
+eyy]ewq;[wq
+wq
+]weq'ауфйкацй'
+\
+s
+s
+s])
+{
+    /* code */
 }
+
+}
+
+
+
+//я ебал ваших матерей)))
